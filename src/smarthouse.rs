3aa5d116e6c7250183baa09096dd556216408a33
piuu-piuu <91653435+piuu-piuu@users.noreply.github.com>
2022-06-27 19:37:02 +0500

    fixate master

--- src/smarthouse.rs
+++ src/smarthouse.rs
@@ -4,12 +4,12 @@ use std::collections::{BTreeMap, HashSet};
 
 const EMPTY_ENTRY: &str = "";
 
-pub struct SmartHouse<'a, 'b> {
+pub struct SmartHouse<'a> {
     pub name: &'a str,
-    pub rooms: BTreeMap<&'b str, HashSet<&'b str>>,
+    pub rooms: BTreeMap<&'a str, HashSet<&'a str>>,
 }
 
-impl SmartHouse<'_, '_> {
+impl<'a> SmartHouse<'a> {
     pub fn new() -> Self {
         SmartHouse {
             name: "User House",
@@ -27,6 +27,23 @@ impl SmartHouse<'_, '_> {
         }
     }
 
+    #[allow(unused_variables)]
+    pub fn new_room(&mut self, room_name: &'a str) {
+        let device_holder = HashSet::from_iter(vec![].into_iter());
+        if !(&self.rooms.contains_key(room_name)) {
+            match self.rooms.insert(room_name, device_holder) {
+                Some(device_holder) => println!("The room is added!"),
+                None => {}
+            }
+        } else {
+            println!("The room exists!")
+        }
+    }
+
+    pub fn add_device(&'a mut self, room_name: &'a str, device_name: &'a str) {
+        todo!()
+    }
+
     pub fn get_rooms(&self) -> impl Iterator<Item = &&str> {
         // Размер возвращаемого массива можно выбрать самостоятельно
         self.rooms.keys()
@@ -42,7 +59,7 @@ impl SmartHouse<'_, '_> {
         }
         println!();
 
-        impl Default for SmartHouse<'_, '_> {
+        impl Default for SmartHouse<'_> {
             fn default() -> Self {
                 Self::new()
             }
@@ -93,6 +110,7 @@ mod tests {
         let house = SmartHouse::new();
         house.print_rooms_with_devices();
     }
+
     #[test]
     fn test_devices() {
         let house = SmartHouse::new();
@@ -100,9 +118,25 @@ mod tests {
         house.devices_at_room("bathroom");
         house.devices_at_room("bedroom");
     }
+
     #[test]
     fn test_house_name() {
         let house = SmartHouse::new();
         println!("{}", house.name);
+    }
+
+    #[test]
+    fn test_new_room() {
+        let mut house = SmartHouse::new();
+        house.new_room("living_room");
+    }
+
+    #[test]
+    fn test_devices_again() {
+        let house = SmartHouse::new();
+        house.devices_at_room("kitchen");
+        house.devices_at_room("bathroom");
+        house.devices_at_room("bedroom");
+        house.devices_at_room("living_room");
     }
 }
