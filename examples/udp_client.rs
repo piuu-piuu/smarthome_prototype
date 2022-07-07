2621a4305d0f0cadbdb576224f224398d6846263
piuu-piuu <91653435+piuu-piuu@users.noreply.github.com>
2022-07-07 22:36:39 +0500

    working udp connectivity

--- examples/udp_client.rs
+++ examples/udp_client.rs
@@ -1,10 +1,8 @@
-// use smarthome_4::devices::*;
-// use smarthome_4::remote::*;
+use smarthome_4::remote::*;
 use std::net::UdpSocket;
+use std::{io, str};
 
 fn main() {
-    let socket = UdpSocket::bind("127.0.0.1:34254").expect("couldn't bind to address");
-    let mut buf = [0; 10];
-    let (number_of_bytes, src_addr) = socket.recv_from(&mut buf).expect("Didn't receive data");
-    println!("{:?}", buf)
+    // "127.0.0.1:8000"
+    read_udp("127.0.0.1:8000", "read")
 }
