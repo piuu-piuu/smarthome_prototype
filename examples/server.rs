85e92777843fa795fae91e29e57860f13d33b9b2
piuu-piuu <91653435+piuu-piuu@users.noreply.github.com>
2022-09-04 11:31:31 +0500

    multiple data sources

--- examples/server.rs
+++ examples/server.rs
@@ -1,3 +1,4 @@
+use mongodb::Client;
 use smarthome_4::models::SmartHouse;
 use std::sync::Mutex;
 
@@ -11,18 +12,24 @@ use smarthome_4::paths;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    // let client = Client::with_uri_str("mongodb://localhost:27017")
-    //     .await
-    //     .expect("failed to connect");
+    let client = Client::with_uri_str("mongodb://localhost:27017")
+        .await
+        .expect("failed to connect");
 
     let house = Data::new(Mutex::new(SmartHouse::new()));
+    let database = Data::new(client.database("house_2"));
 
-    HttpServer::new(move || {
+    let res = HttpServer::new(move || {
         App::new()
             .app_data(Data::clone(&house))
+            .app_data(Data::clone(&database))
             .service(web::resource("/add_room").route(web::post().to(paths::add_room)))
     })
     .bind(("127.0.0.1", 8080))?
     .run()
-    .await
+    .await;
+
+    // write to db here?
+    println!("Yep");
+    res
 }
--- src/paths.rs
+++ src/paths.rs
@@ -1,21 +1,17 @@
 use std::sync::Mutex;
 
 use actix_web::{
-    web::{self},
+    web::{self, Data},
     HttpResponse,
 };
+use mongodb::Database;
 
 use crate::models::SmartHouse;
-
-// #[post("/add_room")]
-// pub async fn add_room(house: &mut SmartHouse, new_room: web::Path<String>) -> HttpResponse {
-//     SmartHouse::insert_room(house, new_room.into_inner());
-//     HttpResponse::Ok().json("New room added.")
-// }
 
 pub async fn add_room(
     house: web::Data<Mutex<SmartHouse>>,
     new_room: web::Path<String>,
+    db: Data<Database>,
 ) -> HttpResponse {
     let mut da_haus = house.lock().unwrap();
     da_haus.insert_room(new_room.into_inner());
