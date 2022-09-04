c23f068cd9fcfaa1196b9497f2a4492c7645baea
piuu-piuu <91653435+piuu-piuu@users.noreply.github.com>
2022-09-04 16:24:50 +0500

    getting device state

--- src/paths.rs
+++ src/paths.rs
@@ -1,27 +1,13 @@
-use std::sync::Mutex;
-
 use actix_web::{
     web::{self, Data},
     HttpResponse,
 };
 use mongodb::{bson::doc, Database};
+use std::sync::Mutex;
 
 #[allow(unused_imports)]
 use crate::devices::{SmartSocket, SmartThermometer};
 use crate::models::SmartHouse;
-
-#[allow(unused_variables)]
-pub async fn add_room(
-    house: web::Data<Mutex<SmartHouse>>,
-    new_room: web::Path<String>,
-    db: Data<Database>,
-) -> HttpResponse {
-    let mut da_haus = house.lock().unwrap();
-    da_haus.insert_room(new_room.into_inner());
-    // getting result
-    let result = da_haus.clone();
-    HttpResponse::Ok().json(result)
-}
 
 #[allow(unused_variables)]
 pub async fn db_commit(house: web::Data<Mutex<SmartHouse>>, db: Data<Database>) -> HttpResponse {
@@ -42,4 +28,31 @@ pub async fn db_commit(house: web::Data<Mutex<SmartHouse>>, db: Data<Database>) 
         .map_err(|err| format!("DB_ERROR: {}", err))
         .expect("Commit error.");
     HttpResponse::Ok().json("Commited.")
+}
+
+pub async fn add_room(
+    house: web::Data<Mutex<SmartHouse>>,
+    new_room: web::Path<String>,
+) -> HttpResponse {
+    let mut da_haus = house.lock().unwrap();
+    da_haus.insert_room(new_room.into_inner());
+    // getting result
+    let result = da_haus.clone();
+    HttpResponse::Ok().json(result)
+}
+
+pub async fn get_data(
+    house: web::Data<Mutex<SmartHouse>>,
+    room: web::Path<String>,
+    device: web::Path<String>,
+) -> HttpResponse {
+    // let mut da_haus = house.lock().unwrap();
+    // getting result
+    let mut result = "None".to_string();
+    match device.into_inner().as_str() {
+        "socket" => result = SmartSocket::get_data(),
+        "thermometer" => result = SmartThermometer::get_data(),
+        _ => (),
+    }
+    HttpResponse::Ok().json(result)
 }
